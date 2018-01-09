11def499706b1b8b20be9caa37b0d79f9b87c1fb
Andrey Lushnikov <lushnikov@chromium.org>
2018-01-09 10:11:17 -0800

    support safari9

---
+++ pushups.js
@@ -0,0 +1,16 @@
+var progress = [
+  {
+    date: 'Jan 9, 2018',
+    andrey: 0+10,
+    sergey: 0
+  },
+  {
+    date: 'Jan 8, 2018',
+    andrey: 100,
+    sergey: 100
+  },
+];
+
+for (const entry of progress)
+  entry.date = new Date(entry.date);
+
--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-const START = new Date('Jan 8, 2018'); // Inclusive
-const END = new Date('Feb 7, 2018'); // Exclusive
-const MS_IN_DAY = 24 * 60 * 60 * 1000;
-const TODAY = new Date();
-const YESTERDAY = new Date(Date.now() - MS_IN_DAY);
-const TOTAL_DAYS = Math.round((END.getTime() - START.getTime()) / MS_IN_DAY);
-const PUSHUPS_DAYILY = 100;
-const TOTAL_PUSHUPS = PUSHUPS_DAYILY * TOTAL_DAYS;
+var START = new Date('Jan 8, 2018'); // Inclusive
+var END = new Date('Feb 7, 2018'); // Exclusive
+var MS_IN_DAY = 24 * 60 * 60 * 1000;
+var TODAY = new Date();
+var YESTERDAY = new Date(Date.now() - MS_IN_DAY);
+var TOTAL_DAYS = Math.round((END.getTime() - START.getTime()) / MS_IN_DAY);
+var PUSHUPS_DAYILY = 100;
+var TOTAL_PUSHUPS = PUSHUPS_DAYILY * TOTAL_DAYS;
 
-const progress = [
-  { date: 'Jan 9, 2018', a: 0+10, s: 0},
-  { date: 'Jan 8, 2018', a: 100, s: 100},
-];
-
-for (const entry of progress)
-  entry.date = new Date(entry.date);
-
-window.addEventListener('DOMContentLoaded', () => {
-  let daysIn = 0;
-  for (let date = START; date < TODAY && date < END; date = new Date(date.getTime() + MS_IN_DAY))
+window.addEventListener('DOMContentLoaded', function () {
+  var daysIn = 0;
+  for (var date = START; date < TODAY && date < END; date = new Date(date.getTime() + MS_IN_DAY))
     ++daysIn;
   document.querySelector('.subtitle').textContent = `Day ${daysIn} of ${TOTAL_DAYS}`;
 
-  let aTotal = 0;
-  let sTotal = 0;
-  let aToday = 0;
-  let sToday = 0;
-  for (const entry of progress) {
+  var aTotal = 0;
+  var sTotal = 0;
+  var aToday = 0;
+  var sToday = 0;
+  for (var entry of progress) {
     if (entry.date <= TODAY) {
-      aTotal += entry.a;
-      sTotal += entry.s;
+      aTotal += entry.andrey;
+      sTotal += entry.sergey;
     }
     if (entry.date > YESTERDAY && entry.date <= TODAY) {
-      aToday = entry.a;
-      sToday = entry.s;
+      aToday = entry.andrey;
+      sToday = entry.sergey;
     }
   }
 
@@ -44,19 +36,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
   requestAnimationFrame(() => {
     {
-      const progressToday = document.querySelector('.a .progress-today .progress-fill');
+      var progressToday = document.querySelector('.a .progress-today .progress-fill');
       progressToday.style.setProperty('width', Math.round(aToday * 100 / PUSHUPS_DAYILY) + '%');
-      const progressTotal = document.querySelector('.a .progress-total .progress-fill');
+      var progressTotal = document.querySelector('.a .progress-total .progress-fill');
       progressTotal.style.setProperty('width', Math.round(aTotal * 100 / TOTAL_PUSHUPS) + '%');
     };
     {
-      const progressToday = document.querySelector('.s .progress-today .progress-fill');
+      var progressToday = document.querySelector('.s .progress-today .progress-fill');
       progressToday.style.setProperty('width', Math.round(sToday * 100 / PUSHUPS_DAYILY) + '%');
-      const progressTotal = document.querySelector('.s .progress-total .progress-fill');
+      var progressTotal = document.querySelector('.s .progress-total .progress-fill');
       progressTotal.style.setProperty('width', Math.round(sTotal * 100 / TOTAL_PUSHUPS) + '%');
     }
   });
-});
-
-function handlePerson(name) {
-}
+}, false);
